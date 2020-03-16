aa9607a74e05d9203f65ad6f74cc8cf1c7ba2413
Takatoshi Wada <fueledbytaching@gmail.com>
2020-03-16 14:14:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
+export default {
+  name: "app",
+  components: {
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Takatoshi Wada",
+          email: "fueledbytaching@gmail.com"
+        },
+        {
+          id: 2,
+          name: "Yamada Tarou",
+          email: "yamadatarou@gmail.com"
+        },
+        {
+          id: 3,
+          name: "Lloyd Andrew Bernas",
+          email: "lloydandrewbernas@gmail.com"
+        }
+      ]
+    };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+      this.employees = [...this.employees, newEmployee];
+      console.log(this.employees);
+    },
+    deleteEmployee(id) {
+      this.employees = this.employees.filter(employee => employee.id !== id);
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
